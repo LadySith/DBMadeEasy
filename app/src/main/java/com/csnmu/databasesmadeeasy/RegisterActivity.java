f5cd2472f0f6d2d7bce4f10df25bda99a62f0781
Sithe Ncube <me.sithe.ncube@gmail.com>
2018-12-06 23:04:51 +0200

    Updating repository

---
+++ app/src/main/java/com/csnmu/databasesmadeeasy/RegisterActivity.java
@@ -0,0 +1,115 @@
+package com.csnmu.databasesmadeeasy;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+@SuppressWarnings("StatementWithEmptyBody")
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextConfirmPassword;
+    private Button buttonSignup;
+    private TextView textViewSignIn;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        editTextEmail = (EditText) findViewById(R.id.ed_Email);
+        editTextPassword = (EditText) findViewById(R.id.ed_Password);
+        editTextConfirmPassword = (EditText) findViewById(R.id.ed_ConfirmPassword);
+        buttonSignup = (Button) findViewById(R.id.bt_Reg);
+        textViewSignIn = (TextView) findViewById(R.id.tv_SignIn);
+
+        buttonSignup.setOnClickListener(this);
+        textViewSignIn.setOnClickListener(this);
+    }
+
+    private void registerUser() {
+
+        //getting email and password from edit texts
+        String email = editTextEmail.getText().toString().trim();
+        String password  = editTextPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+        //checking if email and passwords are empty
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(password != confirmPassword) {
+            Toast.makeText(this,"Passwords do not match",Toast.LENGTH_LONG).show();
+        }
+
+        //if the email and password are not empty
+        //displaying a progress dialog
+
+        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+                            //display some message here
+                            Toast.makeText(RegisterActivity.this,"Successfully registered",Toast.LENGTH_LONG).show();
+                        }else{
+                            //display some message here
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(RegisterActivity.this,"User already registered",Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(RegisterActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSignup){
+            registerUser();
+        }
+
+        if (view == textViewSignIn) {
+            //open Sign In Activity
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+}
